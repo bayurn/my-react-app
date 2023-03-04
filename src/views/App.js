369850467e1b3278b33af844c93c3fe6917e468c
bayurn <bayurizkynurseto19@gmail.com>
2023-03-04 22:19:52 +0700

    add new section to App js

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,13 @@
 import { Navbar } from "../components/index";
-import { Banner, Caption, Featured } from "../views/src/index";
+import { Banner, Caption, Featured, Highlight } from "../views/src/index";
 import data from "./data-featured.json";
 import { StyledSection } from "./App-style";
 
 const imgBanner =
   "https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80";
+
+const imgHighlight =
+  "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80";
 
 const Home = () => {
   return (
@@ -13,6 +16,7 @@ const Home = () => {
       <Banner image={imgBanner} />
       <Caption />
       <Featured data={data.data} />
+      <Highlight image={imgHighlight} />
     </StyledSection>
   );
 };
