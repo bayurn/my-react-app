3f5092f0cf201b7a40dbb0cea1351ec2b209f75a
bayurn <bayurizkynurseto19@gmail.com>
2023-03-07 21:48:35 +0700

    add new section to app

--- src/views/App.js
+++ src/views/App.js
@@ -6,12 +6,15 @@ import {
   Highlight,
   Gallery,
   Event,
+  PromoBanner,
 } from "../views/src/index";
 import data from "./data-featured.json";
 import { StyledSection } from "./App-style";
 
 const Home = () => {
-  const { featured, banner, highlight, gallery, event } = data.data;
+  const { featured, banner, highlight, gallery, event, promoBanner } =
+    data.data;
+
   return (
     <StyledSection>
       <Navbar />
@@ -21,6 +24,7 @@ const Home = () => {
       <Highlight data={highlight} />
       <Gallery data={gallery} />
       <Event data={event} />
+      <PromoBanner data={promoBanner} />
     </StyledSection>
   );
 };
--- src/views/src/index.js
+++ src/views/src/index.js
@@ -4,3 +4,4 @@ export { default as Featured } from "./section-featured";
 export { default as Highlight } from "./section-highlight";
 export { default as Gallery } from "./section-gallery";
 export { default as Event } from "./section-event";
+export { default as PromoBanner } from "./section-promo-banner";
