2c385f3a27d96acfb92d4af3864ce74e23d3e787
bayurn <bayurizkynurseto19@gmail.com>
2023-03-09 21:21:11 +0700

    update footer

--- src/components/src/Footer/index.js
+++ src/components/src/Footer/index.js
@@ -1,11 +1,10 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { StyledFooter, StyledWrapperMenu, StyledMenuList } from "./style";
 
 export const Footer = () => {
   const company = {
     image: "",
-    description: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed erat placerat, ultricies lacus ac, ultricies libero. Phasellus placerat scelerisque nisi, sit amet ullamcorper tellus ultricies at. Phasellus sit amet libero non nisi varius ultrices vitae eu diam. Mauris urna mauris, aliquet sed quam ut, imperdiet aliquam mi. Integer molestie sit amet nunc nec tincidunt.",
     socialMedia: [
       {
         icons: "fa-brands fa-facebook",
@@ -19,22 +18,95 @@ export const Footer = () => {
     ],
   };
 
+  const explore = [
+    {
+      title: "Hotel 5 Star",
+    },
+    {
+      title: "Apartment",
+    },
+    {
+      title: "Environment",
+    },
+    {
+      title: "Facilities",
+    },
+  ];
+
+  const blog = [
+    {
+      title: "Book Room",
+    },
+    {
+      title: "Rent Car",
+    },
+    {
+      title: "Pool",
+    },
+    {
+      title: "Food",
+    },
+  ];
+
+  const about = [
+    {
+      title: "Our hotel",
+    },
+    {
+      title: "History",
+    },
+    {
+      title: "Contact Us",
+    },
+    {
+      title: "Branch",
+    },
+  ];
+
   return (
     <StyledFooter>
       <div className="container">
         <StyledWrapperMenu>
-          <StyledMenuList>
-            <div className="wr-logo">
-              <FontAwesomeIcon icon="fa-solid fa-square-h" />
-              <h2>Macarena Hotel</h2>
-            </div>
-            <h3>{company.description}</h3>
-            <div className="ic-socmed">
-              {company.socialMedia.map((item) => {
-                return <FontAwesomeIcon icon={item.icons} />;
-              })}
-            </div>
-          </StyledMenuList>
+          <div className="wr-menu">
+            <StyledMenuList>
+              <div className="wr-logo">
+                <h2>Macarena Hotel</h2>
+              </div>
+              <span>{company.description}</span>
+              <div className="ic-socmed">
+                {/* {company.socialMedia.map((item) => {
+                return (
+                );
+              })} */}
+              </div>
+            </StyledMenuList>
+          </div>
+          <div className="menu-right">
+            <StyledMenuList>
+              <div className="wrapper">
+                <h3>EXPLORE</h3>
+                {explore.map((list, key) => {
+                  return <span key={key}>{list.title}</span>;
+                })}
+              </div>
+            </StyledMenuList>
+            <StyledMenuList>
+              <div className="wrapper">
+                <h3>BLOG</h3>
+                {blog.map((list, key) => {
+                  return <span key={key}>{list.title}</span>;
+                })}
+              </div>
+            </StyledMenuList>
+            <StyledMenuList>
+              <div className="wrapper">
+                <h3>ABOUT</h3>
+                {about.map((list, key) => {
+                  return <span key={key}>{list.title}</span>;
+                })}
+              </div>
+            </StyledMenuList>
+          </div>
         </StyledWrapperMenu>
       </div>
     </StyledFooter>
--- src/components/src/Footer/style.js
+++ src/components/src/Footer/style.js
@@ -4,19 +4,44 @@ export const StyledFooter = Styled.div`
   // background-color: #2C3333;
   margin-top: 32px;
   border-top: 1px solid #2C3333;
+  padding: 16px 0;
 `;
 
 export const StyledWrapperMenu = Styled.div`
   display: grid;
-  grid-template-columns: auto auto auto auto;
+  grid-template-columns: 30% auto auto auto;
+  justify-content: space-between;
+
+  .menu-right {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 export const StyledMenuList = Styled.div`
+  font-family: 'Roboto', sans-serif;
+
   .wr-logo {
     display: flex;
+    h2 {
+      margin: 0 0 20px;
+    }
   }
 
   .ic-socmed {
     display: flex;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    h3 {
+      margin: 0 0 20px;
+    }
+    span {
+      &:not(:last-child) {
+        margin-bottom: 8px;
+      }
+    }
   }
 `;
