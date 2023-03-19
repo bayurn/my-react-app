c06d65c19b324103d05144f49e217a714b9406c3
bayurn <bayurizkynurseto19@gmail.com>
2023-03-19 20:33:58 +0700

    add gradient to img recommendation

--- src/views/src/section-recommendation/index.js
+++ src/views/src/section-recommendation/index.js
@@ -1,25 +1,34 @@
-import { StyledSection, StyledWrapper } from "./style";
+import { StyledSection, StyledWrapper, StyledImg } from "./style";
 
 const SectionRecommend = () => {
+  const Main = () => {
+    return (
+      <StyledSection>
+        <StyledWrapper>
+          <StyledImg
+            src="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="Beach"
+            width="100%"
+            height="100%"
+          />
+        </StyledWrapper>
+        <StyledWrapper>
+          <StyledImg
+            src="https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+            alt="Mountain"
+            width="100%"
+            height="100%"
+          />
+        </StyledWrapper>
+      </StyledSection>
+    );
+  };
+
   return (
-    <StyledSection className="container">
-      <StyledWrapper>
-        <img
-          src="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-          alt="Beach"
-          width="100%"
-          height="100%"
-        />
-      </StyledWrapper>
-      <StyledWrapper>
-        <img
-          src="https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
-          alt="Mountain"
-          width="100%"
-          height="100%"
-        />
-      </StyledWrapper>
-    </StyledSection>
+    <div className="container">
+      <h1>Recommendation</h1>
+      <Main />
+    </div>
   );
 };
 
--- src/views/src/section-recommendation/style.js
+++ src/views/src/section-recommendation/style.js
@@ -7,16 +7,37 @@ export const StyledSection = Styled.div`
 `;
 
 export const StyledWrapper = Styled.div`
-  &:last-child {
-    padding-left: 10px;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0px;
+    top: 0;
+    width: calc(100% - 300px);
+    max-width: 256px;
+    height: 100%;
+    max-height: 350px;
+    display: inline-block;
+    background: linear-gradient(-90deg, rgba(0, 0, 0, 0) 21%, rgb(0, 0, 0) 100%);
+    opacity: 0.9;
   }
+
+  // &:last-child {
+  //   padding-left: 10px;
+  // }
 
   &:first-child {
     padding-right: 10px;
   }
 
-  img {
-    height: 350px;
-    object-fit: cover;
-  }
+  // &:last-child {
+  //   margin-left: 6px;
+  //   left: 4px;
+  // }
+`;
+
+export const StyledImg = Styled.img`
+  height: 350px;
+  object-fit: cover;
 `;
