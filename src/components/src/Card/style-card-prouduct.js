dac9f0711689d4558304778c92ca748dc29d38ac
bayurn <bayurizkynurseto19@gmail.com>
2023-03-29 22:48:39 +0700

    create style for card product

---
+++ src/components/src/Card/style-card-prouduct.js
@@ -0,0 +1,10 @@
+import Styled from "styled-components";
+
+export const StyledSection = Styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledButton = Styled.button`
+
+`;
