2d0fd09d2efb66ec37ea4b4a5feda37a0683cbf8
bayurn <bayurizkynurseto19@gmail.com>
2023-03-30 22:12:00 +0700

    add card product to export

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Navbar } from "./src/Navbar/container";
 export { default as Banner } from "./src/Banner/container";
-export { Card } from "./src/Card";
+export { Card, CardProduct } from "./src/Card";
 export { Footer } from "./src/Footer";
