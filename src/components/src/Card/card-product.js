614bd534151f00b343d29012827e08c8ae3ee462
bayurn <bayurizkynurseto19@gmail.com>
2023-04-01 23:34:44 +0700

    add new style for wrapper content under image

--- src/components/src/Card/card-product.js
+++ src/components/src/Card/card-product.js
@@ -1,4 +1,8 @@
-import { StyledSection, StyledButton } from "./style-card-prouduct";
+import {
+  StyledSection,
+  StyledButton,
+  StyledContent,
+} from "./style-card-prouduct";
 
 const CardProduct = (props) => {
   const { imgSrc, title, price } = props;
@@ -6,9 +10,11 @@ const CardProduct = (props) => {
   return (
     <StyledSection>
       <img src={imgSrc} alt={title} width="100%" height="100%" />
-      <h2>{title}</h2>
-      <span>{price}</span>
-      <StyledButton>Book Now</StyledButton>
+      <StyledContent>
+        <h2>{title}</h2>
+        <span>{price}</span>
+        <StyledButton>Book Now</StyledButton>
+      </StyledContent>
     </StyledSection>
   );
 };
