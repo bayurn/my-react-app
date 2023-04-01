7fa282227de7bb0648b6fc0194142630c5c71362
bayurn <bayurizkynurseto19@gmail.com>
2023-04-01 23:35:05 +0700

    update style for button, image, & card

--- src/components/src/Card/style-card-prouduct.js
+++ src/components/src/Card/style-card-prouduct.js
@@ -1,20 +1,39 @@
 import Styled from "styled-components";
 
 export const StyledSection = Styled.div`
-  display: flex;
-  flex-direction: column;
+  // display: flex;
+  // flex-direction: column;
   text-align: center;
   border-radius: 8px;
   border: 1px solid gray;
   height: 100%;
-  max-height: 423px;
+  position: relative;
 
   img {
     border-radius: 8px 8px 0 0;
+    width: 100%;
+    height: 228px;
   }
+`;
+
+export const StyledContent = Styled.div`
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const StyledButton = Styled.button`
   height: 46px;
   padding: 8px 12px;
+  border-radius: 8px;
+  border: 1px solid green;
+  background: green;
+  color: white;
+  margin-top: 8px;
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 4px 8px;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.85);
+  }
 `;
